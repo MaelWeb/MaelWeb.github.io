406d0f8c0ec32e6992b7bcb8d4f73fa4bb72df10
Mael <mael@MaelMBP.local>
2016-01-30 08:36:51 +0800

    [update] index loadding

---
+++ src/app/components/main.jsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+
+var Main = React.createClass({
+
+	getInitialState() {
+		return{
+			layout : " ",
+			container : "ip-container loading"
+		};
+	},
+
+	componentDidMount() {
+		this._startLoading();
+	},
+
+ 	render() {
+ 		return (
+ 			<div className={this.state.layout}>
+ 			<div id="ip-container" className={this.state.container}>
+ 				<header className="ip-header">
+					<h1 className="ip-logo">
+						<span className="wihite">U</span>nder 7℃
+					</h1>
+					<div className="ip-loader">
+						<svg className="ip-inner" width="60px" height="60px" viewBox="0 0 80 80">
+							<path className="ip-loader-circlebg" d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z"/>
+							<path ref="ipLoaderCircle" className="ip-loader-circle" d="M40,10C57.351,10,71,23.649,71,40.5S57.351,71,40.5,71 S10,57.351,10,40.5S23.649,10,40.5,10z"/>
+						</svg>
+					</div>
+				</header>
+				<div className="ip-main">
+					<div className="wrap">
+			            <img src="about/images/me.png" alt="my" className="animated zoomIn" />
+			            <h1>MaelWeb</h1>
+			            <h3>Web Engineer</h3>
+			            <a href="/blog">BLOG</a>
+			            <a href="/about"> RESUME</a>
+        			</div>
+				</div>
+			</div>
+			</div>
+ 		);
+ 	},
+
+ 	_startLoading() {
+ 		var circle = ReactDom.findDOMNode(this.refs.ipLoaderCircle);
+ 		var progress = 0;
+ 		let self = this;
+ 		circle.style.strokeDasharray = circle.style.strokeDashoffset = circle.getTotalLength();
+ 		var interval = setInterval(function(){
+			 				progress = Math.min( progress + Math.random() * 0.1, 1 );
+			 				circle.style.strokeDashoffset = circle.getTotalLength() * ( 1 - progress );
+			 				if ( progress === 1 ) {
+								clearInterval( interval );
+								self._changClass();
+							};
+			 		}, 80);
+ 	},
+
+ 	_changClass() {
+ 		this.setState({
+ 			layout : "layout-switch",
+			container : "ip-container loaded"
+ 		});
+ 	}
+
+});
+
+module.exports = Main;
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 var path = require("path");
 var webpack = require("webpack");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var node_modules = path.resolve(__dirname, 'node_modules');
 
 module.exports = {
-    entry: {
-        app: "./js/main.js",
-    },
+    entry: [
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
+        "./src/app/app.js"
+    ],
     output: {
-        path: path.resolve(__dirname, "pack"),
-        publicPath: "pack",
-        filename: "main.min.js",
+        path: path.resolve(__dirname, "build/"),
+        publicPath: "build/",
+        filename: "app.js",
     },
     resolve: {
         extensions: ['', '.js', '.jsx'],
@@ -23,25 +26,22 @@ module.exports = {
                 presets: ['es2015', 'react']
             }
         }, {
-            test: /\.less$/,
-            loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader!less-loader")
+            test: /\.css?$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader!autoprefixer-loader")
         }, {
             test: /\.(eot|woff|woff2|ttf|svg)$/,
             loader: 'url-loader'
-                // loader: 'url-loader'
         }, {
-            test: /\.(png|jpg|gif)$/,
+            test:  /\.(png|jpg|gif)$/,
             loader: "url-loader?limit=8192&name=/images/[name]-[hash].[ext]"
         }]
     },
     plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin(),
         new webpack.optimize.CommonsChunkPlugin('common.js'),
         new ExtractTextPlugin("style.css", {
             allChunks: false
-        }),
-        new webpack.ProvidePlugin({
-            $: 'jquery',
-            'jQuery': 'jquery'
         })
-    ],
-}
+    ]
+};
